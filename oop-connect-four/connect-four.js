38c8a8ab995c8549e6074971d0495e0998e0398a
kriskerby <kriskerby@gmail.com>
2021-04-07 15:01:42 -0700

    phaseOne

---
+++ oop-connect-four/connect-four.js
@@ -0,0 +1,44 @@
+import Game from "./game.js";
+
+let game = undefined;
+
+function updateUI() {
+  if (game === undefined) {
+    document.getElementById("board-holder").classList.add("is-invisble");
+    const gameName = document.getElementById("game-name");
+    gameName.innerHTML = game.getName();
+  } else {
+    const gameName = document.getElementById("game-name");
+    gameName.innerHTML = game.getName();
+    document.getElementById("board-holder").classList.remove("is-invisble");
+  }
+}
+
+function updateButton() {
+  const player1Name = document.getElementById("player-1-name").value;
+  const player2Name = document.getElementById("player-2-name").value;
+
+  const isGameReady = player1Name.length > 0 && player2Name.length > 0;
+  document.getElementById("new-game").disabled = !isGameReady;
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const player1Name = document.getElementById("player-1-name");
+  const player2Name = document.getElementById("player-2-name");
+
+  player1Name.addEventListener("keyup", (event) => {
+    updateButton();
+  });
+
+  player2Name.addEventListener("keyup", (event) => {
+    updateButton();
+  });
+
+  const newGame = document.getElementById("new-game");
+
+  newGame.addEventListener("click", (event) => {
+    game = new Game(player1Name.value, player2Name.value);
+
+    updateUI();
+  });
+});
---
+++ oop-connect-four/game.js
@@ -0,0 +1,9 @@
+export default class Game {
+    constructor(player1, player2){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+    getName(){
+        return `${this.player1} vs. ${this.player2}`;
+    }
+}
